cf93d57d6b8c62f3e7ca780e82539cd4bd43d48f
Yamataw <maxime.henri@ynov.com>
2023-06-01 16:56:11 +0200

    Search Bar + lat/  long + postal_code

---
+++ src/components/RechercheVille.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-autocomplete
+    label="Autocomplete"
+    v-model="ville"
+    id="search"
+    :items="searched"
+    @update:search="search_city"
+    @update:modelValue="select_city"
+    variant="solo"
+  ></v-autocomplete>
+</template>
+
+<script>
+
+export default {
+  name : "RechercheVille",
+  data() {
+    return {
+      searched : [],
+      parsed_searched : [],
+      ville :"",
+      ville_data : {}
+    }
+  },
+
+  methods : {
+
+    select_city(){
+      console.log(this.ville);
+      for (let i = 0; i < this.parsed_searched.length; i++) {
+        if (this.parsed_searched[i].nom === this.ville) {
+          this.ville_data= this.parsed_searched[i]
+        }
+      }
+    },
+
+    search_city() {
+      let str = document.getElementById('search').value
+      var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+
+      fetch(`https://geo.api.gouv.fr/communes?nom=${str}&fields=departement,centre&boost=population&limit=5`, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+          this.parsed_searched = JSON.parse(result);
+          const names = [];
+
+          for (let i = 0; i < this.parsed_searched.length; i++) {
+            names.push(this.parsed_searched[i].nom);
+          }
+          this.searched = names;
+
+        })
+        .catch(error => console.log('error', error));
+
+    }
+  }
+}
+</script>
+

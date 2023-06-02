48188727ff2aab20250180eccba4c4ee79d8a41c
Matys Legroux <mtgroux@gmail.com>
2023-06-02 12:11:41 +0200

    gestion not found cours d'eau

--- src/components/DetailTemperature.vue
+++ src/components/DetailTemperature.vue
@@ -1,10 +1,21 @@
 <template>
   <v-progress-circular v-if="loading" indeterminate></v-progress-circular>
   <v-card v-else class="mx-1 mt-2">
-    <v-row class="ma-1" justify="space-between">
-      <v-col cols="8" class="text-body-1" align-self="center">Température :</v-col>
-      <v-col cols="4" class="text-h5" :style='getColorFromTemperature()'>{{ temperature }}°</v-col>
-    </v-row>
+    <div v-if="nomEau !== null">
+      <v-row class="mx-1 mt-1" justify="space-between">
+        <v-col cols="10" class="text-body-1" align-self="center"><span class="font-weight-bold">{{ nomEau
+        }}</span></v-col>
+      </v-row>
+      <v-row class="ma-1" justify="space-between">
+        <v-col cols="8" class="text-body-1" align-self="center">Température :</v-col>
+        <v-col cols="4" class="text-h5" :style='getColorFromTemperature()'>{{ temperature }}°</v-col>
+      </v-row>
+    </div>
+    <div v-else>
+      <v-card-text class="ma-1" justify="space-between">
+        <span class="text-body-1 font-weight-bold">Pas de cours d'eau trouvé à proximité</span>
+      </v-card-text>
+    </div>
   </v-card>
 </template>
 
@@ -27,13 +38,14 @@ export default {
     return {
       coursDeau: [],
       temperature: null,
+      nomEau: null,
       loading: true,
     }
   },
 
-  watch:{
+  watch: {
     async latitude(newVal, oldVal) {
-      if(oldVal !== newVal){
+      if (oldVal !== newVal) {
         this.temperature = 0
         await this.getTemperature(newVal, this.longitude)
       }
@@ -46,15 +58,17 @@ export default {
 
   methods: {
     async getTemperature(latitude, longitude) {
-      if(latitude!==null && longitude!==null){
+      if (latitude !== null && longitude !== null) {
         const response = await fetch(`https://hubeau.eaufrance.fr/api/v1/temperature/chronique?distance=20&latitude=${latitude}&longitude=${longitude}&size=1&sort=desc`);
         const data = await response.json();
 
         if (data?.data.length > 0) {
           this.coursDeau = data.data;
           this.temperature = this.coursDeau[0].resultat.toFixed(1);
+          this.nomEau = this.coursDeau[0].libelle_station
         } else {
           this.coursDeau = [];
+          this.nomEau = null
         }
         this.loading = false
       }
